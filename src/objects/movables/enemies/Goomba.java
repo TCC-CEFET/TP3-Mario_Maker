55788839d3be136ca49890e758531700c608c93c
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2021-08-12 22:38:42 -0300

    Update Goomba.java

--- src/objects/movables/enemies/Goomba.java
+++ src/objects/movables/enemies/Goomba.java
@@ -17,6 +17,7 @@ import objects.movables.* ;
 import objects.statics.* ;
 import singletons.* ;
 
+// Classe do goomba
 public class Goomba extends Enemy {
 	public Goomba(int x, int y, Direction direction) {
 		super(x, y, GoombaSingleton.getInstance().getWidth(), GoombaSingleton.getInstance().getHeight(), direction) ;
@@ -24,11 +25,11 @@ public class Goomba extends Enemy {
 	
 	@Override
 	public boolean verifyPosition(GameObject object, ArrayList<MovableObject> movableList) {
-		if (object.getClass() == Player.class) {
-			if (hitBox.overlaps(((Player) object).getBottomHitBox()) && !hitBox.overlaps(((Player) object).getLeftHitBox()) && !hitBox.overlaps(((Player) object).getRightHitBox())) {
+		if (object.getClass() == Player.class) { // Verifica colisao caso seja um player
+			if (hitBox.overlaps(((Player) object).getBottomHitBox()) && !hitBox.overlaps(((Player) object).getLeftHitBox()) && !hitBox.overlaps(((Player) object).getRightHitBox())) { // Morto pelo player
 				remove() ;
 				return true ;
-			} else if (hitBox.overlaps(object.getHitBox()) && !((Player) object).getPlayerState().isIntangible()) {
+			} else if (hitBox.overlaps(object.getHitBox()) && !((Player) object).getPlayerState().isIntangible()) { // Troca de direcao ao bater no player tangivel
 				direction = direction == Direction.LEFT ? Direction.RIGHT : Direction.LEFT ;
 			}
 		}
