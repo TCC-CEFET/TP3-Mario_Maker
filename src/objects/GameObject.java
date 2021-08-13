b34084056f1a80367730b82eab46220b237a98bb
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2021-08-12 23:01:53 -0300

    Update GameObject.java

--- src/objects/GameObject.java
+++ src/objects/GameObject.java
@@ -12,15 +12,21 @@ import objects.movables.* ;
 import objects.statics.* ;
 import singletons.* ;
 
+// Classe para os objetos do jogo
 public abstract class GameObject {
-	protected Rectangle hitBox ;
+	protected Rectangle hitBox ; // hitBox do objeto
 	
 	public GameObject(int x, int y, int width, int height) {
 		hitBox = new Rectangle(x, y, width, height) ;
 	}
 	
+	// Verifica a posicao e executa determinadas funcoes
 	public abstract boolean verifyPosition(GameObject object, ArrayList<MovableObject> movableList) ;
+	
+	// Atualiza o que precisar
 	public abstract void update() ;
+	
+	// Seta a posicao
 	public void setPosition(Float x, Float y) {
 		if (x == null && y == null) {
 			x = hitBox.x ;
@@ -34,12 +40,18 @@ public abstract class GameObject {
 		hitBox.x = x ;
 		hitBox.y = y ;
 	}
+	
+	// Desenha em tela
 	public abstract void draw(SpriteBatch batch) ;
+	
 	public Rectangle getHitBox() {
 		return hitBox ;
 	}
+	
+	// Funcao para ser chamada quando remover
 	public abstract void remove() ;
 	
+	// Funcao para categorizar o superClass como gameObject
 	public Class<?> getSuperClass() {
 		return GameObject.class ;
 	}
