28a5a1fc0375784d4ec530c0268b185a60faef0b
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2021-08-12 23:29:07 -0300

    Update Launcher.java

--- src/game/Launcher.java
+++ src/game/Launcher.java
@@ -5,16 +5,16 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import singletons.DisplaySingleton;
 
+// Classe com main que lanca a aplicacao
 public class Launcher {
 	public static void main(String[] args) {
+		// Configuration da aplicacao
 		LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
-		configuration.title = "Mario Forever";
+		configuration.title = "Mario™";
 		configuration.useGL20 = false ;
 		configuration.width = DisplaySingleton.getInstance().getWidth() ;
 		configuration.height = DisplaySingleton.getInstance().getHeight() ;
 		
-		new LwjglApplication(new MarioForever(), configuration);
-		
-		System.out.println() ;
+		new LwjglApplication(new MarioForever(), configuration); // Inicializa a aplicacao
 	}
 }
